000e016bb64314b0915f1304c2cd79fe090946c5
adwd <ecdt.mn@gmail.com>
2017-12-20 21:05:54 +0900

    expand chat message column if text too long

--- lib/chat_message.dart
+++ lib/chat_message.dart
@@ -24,18 +24,22 @@ class ChatMessage extends StatelessWidget {
                 child: new Text(_name[0]),
               ),
             ),
-            new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(
-                  _name,
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
-                )
-              ],
+            // Expandedよくわからん・・・
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                    _name,
+                    style: Theme.of(context).textTheme.subhead,
+                    softWrap: true,
+                  ),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  )
+                ],
+              ),
             )
           ],
         ),
