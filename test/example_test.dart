0eb72f16d044bbbeedb3415d6f66edfcd012512e
adwd <adwd@users.noreply.github.com>
2018-01-04 08:43:55 +0900

    add test

--- test/example_test.dart
+++ test/example_test.dart
@@ -1,8 +1,14 @@
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:friendly_chat/main.dart';
 
 void main() {
   test('my first unit test', () {
     var answer = 42;
     expect(answer, 42);
+  });
+
+  testWidgets('app should work', (tester) async {
+    await tester.pumpWidget(new FriendlychatApp());
+    expect(find.text('Chat'), findsOneWidget);
   });
 }
